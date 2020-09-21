66b6d10048f103126c59f43e877bdbf3710a235a
Brian Liu <brian.b.liu@gmail.com>
2020-09-20 21:58:18 -0600

    Add test it can hire employees.

--- test/department_test.rb
+++ test/department_test.rb
@@ -17,4 +17,15 @@ class DepartmentTest < Minitest::Test
     assert_equal "Customer Service", customer_service.name
     assert_equal [], customer_service.employees
   end
+
+  def test_it_can_hire
+    customer_service = Department.new("Customer Service")
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})
+
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron)
+
+    assert_equal [bobbi. aaron], customer_service.employees
+  end
 end
